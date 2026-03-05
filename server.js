97ce12d9565557f36ef13eb0c41174b2d2055b21
Henrique-fern <a23270@alunos.ipca.pt>
2026-03-05 15:26:40 +0000

    update port

--- server.js
+++ server.js
@@ -176,5 +176,5 @@ app.delete("/sports/:id", async (req, res) => {
 
 app.get("/health", (_req, res) => res.json({ ok: true }));
 
-const port = process.env.PORT || 4010;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Sports service on :${port}`));
